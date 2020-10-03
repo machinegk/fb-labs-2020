9aa1b2ff2d5647f8aaef600dae51d2af7f995182
hudson <57281917+machinegk@users.noreply.github.com>
2020-10-03 12:13:11 +0300

    added opportunity to count frequency of each letter

--- cp_1/Blyzniuk_FB-81_cp1/main.py
+++ cp_1/Blyzniuk_FB-81_cp1/main.py
@@ -5,6 +5,13 @@ file = open("text.txt", 'r')
 spaceless_file = file.read()
 spaceless_file = re.sub(r"\W", "", spaceless_file)
 
+letters_amount = len (spaceless_file)
 counted_letters = Counter(spaceless_file)
+
+print(counted_letters)
+print(letters_amount)
+
+for letter in counted_letters:
+    print("Frequency of " + letter + ' = ' + str(counted_letters[letter]/letters_amount))
 
 file.close()
