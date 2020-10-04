87a3789a53306729fefd5067d49870c410115f7a
hudson <57281917+machinegk@users.noreply.github.com>
2020-10-04 20:54:31 +0300

    a lot of different stuff done

--- cp_1/Blyzniuk_Myshkin_FB-81_cp1/funcs.py
+++ cp_1/Blyzniuk_Myshkin_FB-81_cp1/funcs.py
@@ -10,28 +10,21 @@ def extra_remover(file):
     return space_file, spaceless_file
 
 
-def monogram(space_file):
-    s_letters_amount = len(space_file)  # Get amount of symbols in file with spaces
-    s_counted_letters = Counter(space_file)  # Get a dict with frequency of each letter
+def monogram(space_file, spaceless_file):
 
-    s_counted_letters = OrderedDict(sorted(s_counted_letters.items(), key=lambda t: t[0]))  # Sorts the dict
-
-    entropy, frequency_dict = stuff_counter(s_counted_letters,
-                                            s_letters_amount)  # Get overall entropy and counted frequency of each
-    # letter
+    entropy = entropy_counter(space_file) * 2
 
     print("Entropy with spaces = " + str(entropy))  # Print the result
     print("R = " + str(1 - (entropy / math.log(32, 2))))  # Print the redundancy
 
-    ent = frequency_dict[' '] * math.log(frequency_dict[' '], 2)  # Count the entropy of whitespaces
+    entropy = entropy_counter(spaceless_file) * 2
 
-    entropy += ent  # Get rid of that value in our overall entropy
     print("Entropy without spaces = " + str(entropy))  # Print entropy of the text without whitespaces
     print("R = " + str(1 - (entropy / math.log(32, 2))))  # Count the redundancy without whitespaces
 
 
 def bigram(space_file, spaceless_file):
-    s_letter_pairs = re.findall(r'..', space_file)
+    s_letter_pairs = re.findall(r'..?', space_file)
     entropy = entropy_counter(s_letter_pairs)
     print("Entropy with spaces = " + str(entropy))  # Print that value
     print("R = " + str(1 - (entropy / math.log(32, 2))))  # Count the redundancy
@@ -42,7 +35,7 @@ def bigram(space_file, spaceless_file):
     print("Entropy with spaces (crossed) = " + str(entropy))  # Print that value
     print("R = " + str(1 - (entropy / math.log(32, 2))))  # Count the redundancy
 
-    letter_pairs = re.findall(r'..', spaceless_file)
+    letter_pairs = re.findall(r'..?', spaceless_file)
     entropy = entropy_counter(letter_pairs)
     print("Entropy without spaces = " + str(entropy))  # Print that value
     print("R = " + str(1 - (entropy / math.log(32, 2))))  # Count the redundancy
@@ -59,7 +52,7 @@ def stuff_counter(counted, amount):
 
     for element in counted:
         frequency_dict[element] = counted[element] / amount  # Count frequency for each element
-        print("Frequency of " + element + ' = ' + str(frequency_dict[element]))  # Print frequency for each element
+        print( element + ' = ' +  str( " %.5f" % frequency_dict[element]))  # Print frequency for each element
 
     entropy = 0  # Define starting value of entropy
 
@@ -70,12 +63,12 @@ def stuff_counter(counted, amount):
     return entropy, frequency_dict  # Return value of entropy and frequency dictionary
 
 
-def entropy_counter(pairs):
-    bigrams = Counter(pairs)  # Count the frequency of each pair of letters in the text
-    bigrams = OrderedDict(sorted(bigrams.items(), key=lambda t: t[0]))  # Sorts the dict
-    bigrams_amount = len(pairs)  # Return amount of pair of the letters
+def entropy_counter(counted):
+    grams = Counter(counted)  # Count the frequency of each pair of letters in the text
+    grams = OrderedDict(sorted(grams.items(), key=lambda t: t[0]))  # Sorts the dict
+    grams_amount = len(counted)  # Return amount of pair of the letters
 
-    entropy, frequency_dict = stuff_counter(bigrams, bigrams_amount)  # Return overall entropy for the pairs with
+    entropy, frequency_dict = stuff_counter(grams, grams_amount)  # Return overall entropy for the pairs with
 
     # whitespaces and the dict with frequency of
     # each pair
--- cp_1/Blyzniuk_Myshkin_FB-81_cp1/main.py
+++ cp_1/Blyzniuk_Myshkin_FB-81_cp1/main.py
@@ -4,7 +4,7 @@ file = open("text.txt", 'r', encoding='UTF-8') # Open file with proper encoding
 
 space_file, spaceless_file = extra_remover(file)
 
-monogram(space_file)
+monogram(space_file, spaceless_file)
 
 bigram(space_file, spaceless_file)
 
