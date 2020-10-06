9a17fde61d885a8200ca0604834572a97a4441b8
Lord <murmyshkin777@gmail.com>
2020-10-06 19:29:16 +0300

    ciphering texts

--- cp_2/Blyzniuk_Myshkin_FB-81_cp2/main.py
+++ cp_2/Blyzniuk_Myshkin_FB-81_cp2/main.py
@@ -1,6 +1,17 @@
 import re
 
+def cipherWith(key, numberText, alphabet):
 
+    returnedNumberText = numberText
+    keyLen = len(key)
+    alphLen = len(alphabet)
+    for i in range(0, len(returnedNumberText)):
+        returnedNumberText[i] = ( returnedNumberText[i] + indexed_alphabet[ (key[i % keyLen]) ] ) % alphLen
+        returnedNumberText[i] += 1072
+    
+    returnedNumberText = ''.join(map(lambda letter: chr(letter), returnedNumberText))
+    return returnedNumberText
+    
 
 
 file = open('text.txt', 'r', encoding='UTF-8')
@@ -14,24 +25,12 @@ print(text)
 alphabet = list(map(lambda letter: chr(letter), range(1072, 1104)))
 
 
-key1 = 'да'
-key2 = 'кот'
-key3 = 'лорд'
-key4 = 'салат'
-key5 = 'вкусныйвидеосалат'
-
+keys = ['да', 'кот', 'лорд', 'салат', 'вкусныйвидеосалат']
 
 indexed_alphabet = {key: v for v, key in enumerate(alphabet)}
-print (indexed_alphabet)
 
-list = list(map(lambda i: indexed_alphabet[i], text))
-print(list)
-keyLen = len(key1)
-alphLen = len(alphabet)
-for i in range(0, len(list)):
-    list[i] = ( list[i] + indexed_alphabet[ (key1[i % keyLen]) ] ) % alphLen
-    
-    
-print(list)
 
-print(indexed_alphabet)
+numberText = list(map(lambda i: indexed_alphabet[i], text))
+
+key = keys[0]
+ciferedText = cipherWith(key, numberText, alphabet)
