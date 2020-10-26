46b042f48d2431d856b8ec156cc2e9590f1ae6de
hudson <57281917+machinegk@users.noreply.github.com>
2020-10-26 13:31:21 +0200

    optimized gcd function

--- cp_3/Blyzniuk_Myshkin_FB-81_cp3/funcs.py
+++ cp_3/Blyzniuk_Myshkin_FB-81_cp3/funcs.py
@@ -1,6 +1,7 @@
 from collections import Counter, OrderedDict
 import math, re
 
+
 def bigram(spaceless_file):
     bigrams = re.findall(r'..?', spaceless_file)
 
@@ -23,32 +24,29 @@ def bigram(spaceless_file):
 
     return frequency_dict, bigrams
 
+
 def russian_alphabet(exeptions):
     alp = list(map(lambda letter: ord(letter), exeptions))
     alphabet = list(map(lambda letter: chr(letter), [ind for ind in range(1072, 1104) if ind not in alp]))
     return alphabet
 
+
 def indexed_alphabet(alphabet):
     return {key: v for v, key in enumerate(alphabet)}
 
-def gcd(first_number, second_number):
-    print("gcd of " + str(first_number) + " and " + str(second_number), end=" is ")
-    while second_number != 0:
-        t = second_number
-        second_number = first_number % second_number
-        first_number = t
-    print(first_number)
-    return first_number
 
-def gcd_coefs(first_number, second_number):
+def gcd(first_number, second_number):
     coefs = []
+    print("gcd of " + str(first_number) + " and " + str(second_number), end=" is ")
     while second_number != 0:
         t = second_number
         if first_number > second_number:
             coefs.append((first_number - (first_number % second_number)) / second_number)
         second_number = first_number % second_number
         first_number = t
-    return coefs
+    print(first_number)
+    return first_number, coefs
+
 
 def solve_equation(a, b, mod):
     d = gcd(a, mod)
@@ -60,8 +58,9 @@ def solve_equation(a, b, mod):
         else:
             print("0 answers")
 
+
 def find_opposite(a, mod):
-    coefs = gcd_coefs(a, mod)
+    f_number, coefs = gcd(a, mod)
     print(coefs)
     x = 1
     y = 0
