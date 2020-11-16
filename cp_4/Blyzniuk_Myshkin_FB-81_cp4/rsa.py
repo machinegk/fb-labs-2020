ddfbca27f03c55b3ca6d95da6abb9aa5abf96b8f
hudson <57281917+machinegk@users.noreply.github.com>
2020-11-16 10:57:31 +0200

    corrected files

---
+++ cp_4/Blyzniuk_Myshkin_FB-81_cp4/rsa.py
@@ -0,0 +1,26 @@
+def GenerateKeyPair():
+    pass
+
+
+def Encrypt():
+    pass
+
+
+def Decrypt():
+    pass
+
+
+def Sign():
+    pass
+
+
+def Verify():
+    pass
+
+
+def SendKey():
+    pass
+
+
+def ReceiveKey():
+    pass
