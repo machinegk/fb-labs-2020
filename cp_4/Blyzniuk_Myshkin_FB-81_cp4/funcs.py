8014c6b8f94d42b0eacec4cc5e64466e960a7da6
ArMuSebastian <rather.not.say.my.mail@gmail.com>
2020-11-16 20:31:00 +0200

    implementing func groma

--- cp_4/Blyzniuk_Myshkin_FB-81_cp4/funcs.py
+++ cp_4/Blyzniuk_Myshkin_FB-81_cp4/funcs.py
@@ -1,6 +1,7 @@
 import math
 import random
 
+
 def gcd(first_number, second_number):
     coefs = []
     # print("gcd of " + str(first_number) + " and " + str(second_number), end=" is ")
@@ -12,6 +13,7 @@ def gcd(first_number, second_number):
         first_number = t
     # print(first_number)
     return first_number, coefs
+
 
 def find_opposite(a, mod):
     _, coefs = gcd(a, mod)
@@ -28,6 +30,14 @@ def find_opposite(a, mod):
     # print("opposite is: " + str(x))
     return x
 
+
+def func_groma(a, power, mod):
+    result = 1
+    for i in range(0, power):
+        result = result * a % mod
+    return result
+
+
 def random_number(lower_bound, upper_bound):
     return random.randint(lower_bound, upper_bound)
 
@@ -38,6 +48,7 @@ def random_prime(key_length):
     upper_bound = 2 ** (key_length + 1) - 1
     print("Looking for random number in range " + str(lower_bound), str(upper_bound))
     while True:
+        print("k", end="")
         rand_number = random_number(lower_bound, upper_bound)
         if rand_number % 2 == 0:
             rand_number += 1
@@ -50,4 +61,34 @@ def random_prime(key_length):
 
 def miller_rabin_test(number):
     print("Miller rabin test of number " + str(number))
+    k = 100
+    s = 0
+    d = number - 1
+    # number-1 = d * 2^(s) -> d = (number-1) / 2^(s)
+    # s - количество двоек в разложении (number - 1)
+    while d % 2 == 0:
+        s += 1
+        d = d // 2  # d = (d - (d % 2)) /2
+
+    for _ in range(0, k):
+        # take random x in range 1 < x < number OR 2 <= x <= (number-1)
+        x = random_number(2, number - 1)
+        if gcd(x, number)[0] > 1:
+            # number is not prime
+            return False
+        else:
+            # number can be prime or not
+            x =  func_groma(x, d, number) # (x ** d) % number
+            if abs(x) == 1:
+                # sil`no psevdoprostoe za osnovaniem x
+                continue
+            for si in range(1, s-1):
+                # x_r = x^(d * 2^(si)) -> x^(d) * x^(2^si)
+                x_r = (x ** (2 ** si) * x) % number
+                if x_r == -1:
+                    break
+                elif x_r == 1:
+                    return False
+                else:
+                    continue
     return True
--- cp_4/Blyzniuk_Myshkin_FB-81_cp4/main.py
+++ cp_4/Blyzniuk_Myshkin_FB-81_cp4/main.py
@@ -2,3 +2,5 @@ from funcs import *
 from rsa import *
 
 GenerateKeyPair(256)
+
+#print(func_groma(5, 3, 30))
