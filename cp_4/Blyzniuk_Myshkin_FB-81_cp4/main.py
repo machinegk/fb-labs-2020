e02d4112361a2ba46b47d0722377aebdb50cb040
ArMuSebastian <rather.not.say.my.mail@gmail.com>
2020-11-22 18:21:10 +0200

    ready-lab

--- cp_4/Blyzniuk_Myshkin_FB-81_cp4/main.py
+++ cp_4/Blyzniuk_Myshkin_FB-81_cp4/main.py
@@ -2,7 +2,7 @@ from funcs import *
 from rsa import *
 import requests
 
-public_key, private_key = generate_key_pair(25)
+public_key, private_key = generate_key_pair(10)
 print()
 print("Public key: " + "\n\tPublic exponent: " + str(public_key[0]) + "\n\tModulus: " + str(public_key[1]))
 print("Public key(hex): " + "\n\tPublic exponent: " + str(hex(public_key[0])) + "\n\tModulus: " + str(hex(public_key[1])))
@@ -40,17 +40,41 @@ print("----------")
 print()
 
 
-key_length = 20
+key_length = 250
 
 public_key_a, private_key_a = generate_key_pair(key_length)
 public_key_b, private_key_b = generate_key_pair(key_length+1)
 
-a_message = random_number(0, 2 ** key_length)
+print()
+print("A Public key: " + "\n\tPublic exponent: " + str(public_key_a[0]) + "\n\tModulus: " + str(public_key_a[1]))
+print("A Public key(site-hex): " + "\n\tPublic exponent: " + str(site_hex(public_key_a[0])) + "\n\tModulus: " + str(site_hex(public_key_a[1])))
+print()
+print("A Private key: " + "\n\tSecret: " + str(private_key_a[0]) + "\n\tModulus: " + str(private_key_a[1]))
+print("A Private key(site- hex): " + "\n\tSecret: " + str(site_hex(private_key_a[0])) + "\n\tModulus: " + str(site_hex(private_key_a[1])))
+print()
+print("B Public key: " + "\n\tPublic exponent: " + str(public_key_b[0]) + "\n\tModulus: " + str(public_key_b[1]))
+print("B Public key(site-hex): " + "\n\tPublic exponent: " + str(site_hex(public_key_b[0])) + "\n\tModulus: " + str(site_hex(public_key_b[1])))
+print()
+print("B Private key: " + "\n\tSecret: " + str(private_key_b[0]) + "\n\tModulus: " + str(private_key_b[1]))
+print("B Private key(site- hex): " + "\n\tSecret: " + str(site_hex(private_key_b[0])) + "\n\tModulus: " + str(site_hex(private_key_b[1])))
+print()
 
-# k1, s1
+a_message = random_number(0, 2 ** (key_length-1))
+
 povidomlennia = send_key(a_message, private_key_a, public_key_b)
-print(f"User A forms \n\tk1: {povidomlennia[0]}\n\ts1: {povidomlennia[1]}\n")
-
-
 valid_auth = receive_key(povidomlennia, public_key_a, public_key_b, private_key_b)
-print(f"User B check if A signature is valid - {valid_auth}")
+print(f"User B check if A signature is valid - {valid_auth}\n")
+
+str_request = f"http://asymcryptwebservice.appspot.com/rsa/serverKey?keySize=512"
+keygen_request = requests.get(str_request)
+
+print("API public key:" + str(keygen_request.json()))
+server_n = int(keygen_request.json()["modulus"], 16)
+server_e = int(keygen_request.json()["publicExponent"], 16)
+server_public = (server_e, server_n)
+povidomlennia = send_key(a_message, private_key_a, server_public)
+k1, s1 = povidomlennia
+str_request = f"http://asymcryptwebservice.appspot.com/rsa/receiveKey?key={site_hex(k1)}&signature={site_hex(s1)}&modulus={site_hex(public_key_a[1])}&publicExponent={site_hex(public_key_a[0])}"
+
+keygen_request = requests.get(str_request, cookies=keygen_request.cookies)
+print("Answer: " + str(keygen_request.json()))
--- cp_4/Blyzniuk_Myshkin_FB-81_cp4/rsa.py
+++ cp_4/Blyzniuk_Myshkin_FB-81_cp4/rsa.py
@@ -4,7 +4,9 @@ from funcs import *
 def generate_key_pair(key_length):
     # print("Key length is " + str(key_length))
     p = random_prime(key_length)
+    print(f"We found a prime number p = {p}")
     q = random_prime(key_length)
+    print(f"We found a prime number q = {q}")
     while p == q:
         q = random_prime(key_length)
     # print("\nowr p = " + str(p) + "\nand q = " + str(q))
@@ -40,17 +42,16 @@ def verify(message, signature, public_key):
     return success
 
 
-def send_key(a_message, a_private, b_public):
-    s = sign(a_message, a_private)
-    print(f"User A put \n\tk: {s[0]}\n\ts: {s[1]}\n")
-    k1 = left_to_right_power(s[0], b_public[0], b_public[1])
-    s1 = left_to_right_power(s[1], b_public[0], b_public[1])
-    return (k1, s1)
+def send_key(k, a_private, b_public):
+    k1 = encrypt(k, b_public)
+    _, s = sign(k, a_private)
+    _, s1 = sign(s, b_public)
+    return (k1,s1)
 
 
 def receive_key(povidomlennia, public_key_a, public_key_b, private_key_b):
     k1, s1 = povidomlennia
-    k = left_to_right_power(k1, private_key_b[0], public_key_b[1])
-    s = left_to_right_power(s1, private_key_b[0], public_key_b[1])
+    k = decrypt(k1, (private_key_b[0], public_key_b[1]))
+    _, s = sign(s1, (private_key_b[0], public_key_b[1]))
     print(f"User B finds \n\tk: {k}\n\ts: {s}\n")
-    return k == left_to_right_power(s, public_key_a[0], public_key_a[1])
+    return k == decrypt(s, (public_key_a[0], public_key_a[1]))
